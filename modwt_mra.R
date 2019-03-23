83568399df1ab64c9a05f412ef8aec840b5a3170
Nicholas <42080997+Nicholas-Picini@users.noreply.github.com>
2019-03-23 02:05:27 -0400

    Update modwt_mra.R

--- modwt_mra.R
+++ modwt_mra.R
@@ -3,21 +3,29 @@ library(ggplot2)
 library(quantmod)
 setwd("PATH YOU WANT CSV TO SAVE TO")
 
-
+# get stock data from yahoo-finance
 symbol <- "SPY"  
 series <- new.env()
 getSymbols.yahoo(symbol, env = series, from="2009-01-01", to=Sys.Date() + 1, auto.assign=T) 
 stock <- series[[symbol]]
 stock <- stock[, "SPY.Close"]
 
-returns <- na.omit(diff(log(stock)))
+# log returns
+returns <- data.frame(na.omit(diff(log(stock))))
 
-MRA <- mra(returns, "la8", method = 'modwt', 3, boundary = "periodic")
+# level 3 MODWT-MRA using least-asymmetric daubechies wavelet of length 8
+MRA <- mra(returns, "la8", method = 'modwt', 3, boundary = "reflective") # reflective boundary to prevent edge effects
 
 D1 <- MRA[["D1"]]
 D2 <- MRA[["D2"]]
 D3 <- MRA[["D3"]]
 S3 <- MRA[["S3"]]
+
+# remove reflected portion of signal
+D1 <- head(data.frame(D1), -nrow(returns))
+D2 <- head(data.frame(D2), -nrow(returns))
+D3 <- head(data.frame(D3), -nrow(returns))
+S3 <- head(data.frame(S3), -nrow(returns))
 
 df <- data.frame(D1,D2,D3,S3)
 
