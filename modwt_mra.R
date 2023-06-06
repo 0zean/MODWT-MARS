0275902f37962cf1cb23e53bbb0fa8d36fda34bb
Nicholas Picini <npicini@optonline.net>
2023-06-06 04:11:57 -0400

    Update modwt_mra.R

--- modwt_mra.R
+++ modwt_mra.R
@@ -1,13 +1,20 @@
 library(waveslim)
 library(ggplot2)
 library(quantmod)
-setwd("C:/destination/to/save/csv") # path to were you want CSV with MRA results to be saved
 
+# path to were you want CSV with MRA results to be saved
+setwd("C:/destination/to/save/csv")
 
 # get stock data from yahoo-finance
-symbol <- "SPY"  
+symbol <- "SPY"
 series <- new.env()
-getSymbols.yahoo(symbol, env = series, from="2009-01-01", to=Sys.Date() + 1, auto.assign=T) 
+
+getSymbols.yahoo(symbol,
+                 env = series,
+                 from = "2009-01-01",
+                 to = Sys.Date() + 1,
+                 auto.assign = TRUE)
+
 stock <- series[[symbol]]
 stock <- stock[, "SPY.Close"]
 
@@ -16,14 +23,19 @@ returns <- data.frame(na.omit(diff(log(stock))))
 returns <- as.ts(returns)
 
 
-# level 3 MODWT-MRA using least-asymmetric daubechies wavelet (symlet) of length 8
-MRA <- mra(returns, wf = "la8", method = 'modwt', J = 3, boundary = "reflection")
+# level 3 MODWT-MRA using least-asymmetric daubechies wavelet of length 8
+mra <- mra(returns,
+           wf = "la8",
+           method = "modwt",
+           J = 3,
+           boundary = "reflection")
 
-D1 <- MRA[["D1"]]
-D2 <- MRA[["D2"]]
-D3 <- MRA[["D3"]]
-S3 <- MRA[["S3"]]
 
-df <- data.frame(D1,D2,D3,S3)
+d1 <- mra[["D1"]]
+d2 <- mra[["D2"]]
+d3 <- mra[["D3"]]
+d3 <- mra[["S3"]]
 
-write.csv(df, file = 'modwt_mra.csv')
+df <- data.frame(d1, d2, d3, d3)
+
+write.csv(df, file = "modwt_mra.csv")
