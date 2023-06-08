f69e3b26b4e738e701108be930806d4a20219b08
Nicholas Picini <npicini@optonline.net>
2023-06-08 01:12:06 -0400

    Update Python

--- MODWT-MARS.py
+++ MODWT-MARS.py
@@ -1,5 +1,3 @@
-from datetime import date
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -9,19 +7,19 @@ from sklearn.ensemble import AdaBoostRegressor
 from sklearn.metrics import mean_squared_error
 
 # Load MRA detail and scale coefficients
-modwt_mra = pd.read_csv('modwt_mra.csv')
+mra = pd.read_csv("modwt_mra.csv")
 
-d1, d2, d3, s3 = modwt_mra["d1"], modwt_mra["d2"], modwt_mra["d3"], modwt_mra["s3"]
+d1, d2, d3, s3 = mra["d1"], mra["d2"], mra["d3"], mra["s3"]
 
 # Get stock data over same period as MRA
 with open("key.txt") as f:
     key = f.readline().strip("\n")
 
-ts = TimeSeries(key=key, output_format='pandas')
+ts = TimeSeries(key=key, output_format="pandas")
 data, meta_data = ts.get_daily_adjusted("SPY", outputsize="full")
 data = data.loc[data.index >= "2009-01-02"]
 
-price = data['4. close'].iloc[::-1]
+price = data["4. close"].iloc[::-1]
 returns = np.log(price).diff().dropna()
 
 
@@ -35,36 +33,41 @@ data = np.zeros((N - T - (D - 1) * T, D))
 lbls = np.zeros((N - T - (D - 1) * T,))
 
 for t in range((D - 1) * T, N - T):
-    data[t - (D - 1) * T, :] = [series[t - 3 * T], series[t - 2 * T], series[t - T], series[t]]
-    lbls[t - (D - 1) * T] = series[t + T]
+    data[t-(D-1)*T, :] = [series[t-3*T], series[t-2*T], series[t-T], series[t]]
+    lbls[t-(D-1)*T] = series[t+T]
 trnLbls = lbls[:lbls.size - round(lbls.size * 0.3)]
 chkLbls = lbls[lbls.size - round(lbls.size * 0.3):]
 
 
 def MODWT_MARS(series, regressors=4, delay=1):
-    series = series.values
+    src = series.values
 
     D = regressors  # number of regressors
     T = delay  # delay
-    N = len(series)
+    N = len(src)
     data = np.zeros((N - T - (D - 1) * T, D))
     lbls = np.zeros((N - T - (D - 1) * T,))
 
     for t in range((D - 1) * T, N - T):
-        data[t - (D - 1) * T, :] = [series[t - 3 * T], series[t - 2 * T], series[t - T], series[t]]
-        lbls[t - (D - 1) * T] = series[t + T]
+        data[t-(D-1)*T, :] = [src[t-3*T], src[t-2*T], src[t-T], src[t]]
+        lbls[t-(D-1)*T] = src[t+T]
 
-    trnData = data[:lbls.size - round(lbls.size * 0.3), :]
-    trnLbls = lbls[:lbls.size - round(lbls.size * 0.3)]
-    chkData = data[lbls.size - round(lbls.size * 0.3):, :]
-    chkLbls = lbls[lbls.size - round(lbls.size * 0.3):]
+    trnData = data[:lbls.size - round(lbls.size*0.3), :]
+    trnLbls = lbls[:lbls.size - round(lbls.size*0.3)]
+    chkData = data[lbls.size - round(lbls.size*0.3):, :]
+    chkLbls = lbls[lbls.size - round(lbls.size*0.3):]
 
     aa = np.array(chkLbls[-4:]).reshape(1, -1)
     chkData = np.append(chkData, aa, axis=0)
 
     mars = Earth()
     mars.fit(trnData, trnLbls)
-    boosted_mars = AdaBoostRegressor(estimator=mars, n_estimators=50, learning_rate=0.1, loss='exponential')
+
+    boosted_mars = AdaBoostRegressor(estimator=mars,
+                                     n_estimators=50,
+                                     learning_rate=0.1,
+                                     loss="exponential")
+
     boosted_mars.fit(trnData, trnLbls)
     oos_preds = boosted_mars.predict(chkData)
 
@@ -77,25 +80,26 @@ D2_test = pd.DataFrame(MODWT_MARS(d2), columns=["D2"])
 D3_test = pd.DataFrame(MODWT_MARS(d3), columns=["D3"])
 S3_test = pd.DataFrame(MODWT_MARS(s3), columns=["S3"])
 
-D1_test = pd.concat([D1_test, D2_test], axis=1)
-D1_test = pd.concat([D1_test, D3_test], axis=1)
-D1_test = pd.concat([D1_test, S3_test], axis=1)
+pred = pd.concat([D1_test, D2_test, D3_test, S3_test], axis=1)
 
-D1_test['sum'] = D1_test.sum(axis=1)
+pred["sum"] = pred.sum(axis=1)
 
 
-def mda(actual: np.ndarray, predicted: np.ndarray):
+def mda(true: np.ndarray, pred: np.ndarray):
     """ Mean Directional Accuracy """
-    return np.mean((np.sign(actual[1:] - actual[:-1]) == np.sign(predicted[1:] - actual[:-1])).astype(int))
+    return np.mean((np.sign(true[1:] - true[:-1]) == np.sign(pred[1:] - true[:-1])).astype(int))
 
 
-plt.plot(np.array(D1_test["sum"]), color="g")
+# Plot additive prediction against actual returns
+plt.plot(np.array(pred["sum"]), color="g")
 plt.plot(chkLbls)
-plt.annotate("Mean Directional Accuracy = {:.3f}".format(mda(chkLbls, D1_test["sum"][:-1])), (-20, max(chkLbls)+0.01))
-plt.annotate("RMSE = {:.3e}".format(mean_squared_error(chkLbls, D1_test["sum"][:-1], squared=False)), (-20, max(chkLbls)))
+
+plt.annotate("Mean Directional Accuracy = {:.3f}".format(mda(chkLbls, pred["sum"][:-1])), (-20, max(chkLbls)+0.01))
+plt.annotate("RMSE = {:.3e}".format(mean_squared_error(chkLbls, pred["sum"][:-1], squared=False)), (-20, max(chkLbls)))
 plt.legend(["Pred", "Actual"])
 
 figure = plt.gcf()
 figure.set_size_inches(8, 6)
+
 plt.savefig("Results/test.png", dpi=252)
 plt.show()
